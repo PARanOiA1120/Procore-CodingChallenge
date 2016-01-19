acb24ac603c5b2d2ea62dcc70d3efbea8a97fa48
PARanOiA1120 <yuexin@umail.ucsb.edu>
2016-01-19 14:02:02 -0800

    project init

---
+++ Procore-CodingChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Procore-CodingChallenge
+//
+//  Created by Ariel Xin on 1/19/16.
+//  Copyright © 2016 Ariel-gnon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Procore-CodingChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Procore-CodingChallenge
+//
+//  Created by Ariel Xin on 1/19/16.
+//  Copyright © 2016 Ariel-gnon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
